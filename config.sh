2e99335e857b8fdfa16c7043098ed917974cbd28
Carlos Gabriel <oditorrinco222@gmail.com>
2026-01-26 08:52:55 -0300

    added config and script

---
+++ config.sh
@@ -0,0 +1,2 @@
+#mv the config.json to .var/app/org.vinegarhq.Sober/config/sober/ as config.json
+mv /home/carlos/sober-config/config.json /home/carlos/.var/app/org.vinegarhq.Sober/config/sober/config.json
